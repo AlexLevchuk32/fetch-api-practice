9881c6e97b8405fd00f5ba9af709de45c4f39a78
Alex Levchuk <alexlevchuk@mail.ru>
2024-04-04 00:47:52 +0300

    add fetch API query #2, sorted users by names

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,82 @@
 import '../scss/style.scss';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 getUsersData();
+getProductsData();
 
 async function getUsersData() {
 	try {
-		const response = await fetch('https://jsonplaceholder.typicode.com/users');
-		const data = await response.json();
+		const res = await fetch('https://jsonplaceholder.typicode.com/users');
+		const data = await res.json();
 		// console.log(data);
 
-		data.forEach((user) => {
-			const template = `<li class='users__item'>${user.name}</li>`;
+		function sortNames() {
+			const namesArr = [];
 
+			data.forEach((user) => {
+				namesArr.push(user.name);
+			});
+
+			return namesArr.sort();
+		}
+
+		sortNames().forEach((name) => {
+			// console.log(name);
+			const template = `<li class='users__item'>${name}</li>`;
 			document.querySelector('.users__list').insertAdjacentHTML('beforeend', template);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function getProductsData() {
+	try {
+		const res = await fetch('https://dummyjson.com/products');
+		const data = await res.json();
+
+		const products = data.products;
+		// console.log(products);
+		let rows = '';
+
+		products.forEach((product) => {
+			rows += `
+        <tr>
+          <td>
+            ${product.id}
+          </td>
+          <td>
+            ${product.title}
+          </td>
+          <td>
+            ${product.description}
+          </td>
+          <td>
+            ${product.price}
+          </td>
+          <td>
+            ${product.discount}
+          </td>
+          <td>
+            ${product.rating}
+          </td>
+          <td>
+            ${product.stock}
+          </td>
+          <td>
+            ${product.brand}
+          </td>
+          <td>
+            ${product.category}
+          </td>
+          <td>
+            <img src='${product.thumbnail}' title='${product.title}' alt='${product.title}' style='width: 50%;' />
+          </td>
+        </tr>
+      `;
+
+			document.querySelector('.table-rows').insertAdjacentHTML('beforeend', rows);
 		});
 	} catch (error) {
 		console.log(error);
