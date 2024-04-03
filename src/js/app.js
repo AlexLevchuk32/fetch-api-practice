98b7f69e847d2b1dd9bfec5d06fabec2dd03193c
Alex Levchuk <alexlevchuk@mail.ru>
2024-04-03 22:15:27 +0300

    add fetch API query #1

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
+import '../scss/style.scss';
 
+getUsersData();
+
+async function getUsersData() {
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/users');
+		const data = await response.json();
+		// console.log(data);
+
+		data.forEach((user) => {
+			const template = `<li class='users__item'>${user.name}</li>`;
+
+			document.querySelector('.users__list').insertAdjacentHTML('beforeend', template);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
